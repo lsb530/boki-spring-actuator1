3605928c48a5df221967a32d8b6aafeebfb0756c
boki <lsb530@naver.com>
2024-07-08 02:25:52 +0900

    first commit

---
+++ src/main/java/com/boki/bokispringactuator1/BokiSpringActuator1Application.java
@@ -0,0 +1,13 @@
+package com.boki.bokispringactuator1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokiSpringActuator1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokiSpringActuator1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/boki/bokispringactuator1/BokiSpringActuator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.bokispringactuator1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokiSpringActuator1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
