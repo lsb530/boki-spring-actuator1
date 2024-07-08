d00d9c04fa547775ffafb1b8aa6fec2979bcf381
boki <lsb530@naver.com>
2024-07-08 16:48:53 +0900

    parameter 수신 - path variable

--- src/main/java/com/boki/bokispringactuator1/custom/MyLibraryInfoEndpoint.java
+++ src/main/java/com/boki/bokispringactuator1/custom/MyLibraryInfoEndpoint.java
@@ -1,8 +1,12 @@
 package com.boki.bokispringactuator1.custom;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
 import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
 import org.springframework.lang.Nullable;
 
@@ -13,9 +17,28 @@ import java.util.List;
 @Endpoint(id = "my-library-info")
 public class MyLibraryInfoEndpoint {
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @WriteOperation
     public void changeSomething(String name, boolean enableSomething) {
         log.info("name: {}, enableSomething: {}", name, enableSomething);
+    }
+
+    @ReadOperation
+    public ObjectNode getSinglePathTest(@Selector String path) {
+        log.info("path: {}", path);
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("path", path);
+        return objectNode;
+    }
+
+    @ReadOperation
+    public ObjectNode getMultiPathTest(@Selector(match = Selector.Match.ALL_REMAINING) String[] paths) {
+        log.info("paths {}", Arrays.asList(paths));
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.put("paths", Arrays.toString(paths));
+        return objectNode;
     }
 
     @ReadOperation
