453424c708d8d364b63465b2af8c23a4596beb9f
boki <lsb530@naver.com>
2024-07-10 00:46:48 +0900

    health custom endpoint

---
+++ src/main/java/com/boki/bokispringactuator1/health/MyCustomHealthIndicator.java
@@ -0,0 +1,31 @@
+package com.boki.bokispringactuator1.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyCustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        boolean status = getStatus();
+        if (status) {
+            return Health.up()
+                .withDetail("key1", "value1")
+                .withDetail("key2", "value2")
+                .build();
+        }
+        else {
+            return Health.down()
+                .withDetail("key3", "value3")
+                .withDetail("key4", "value4")
+                .build();
+        }
+
+    }
+
+    boolean getStatus() {
+        // 현재시각이 짝수,홀수인지에 따라 up, down을 판단하는 것으로 대체
+        return System.currentTimeMillis() % 2 == 0;
+    }
+}
