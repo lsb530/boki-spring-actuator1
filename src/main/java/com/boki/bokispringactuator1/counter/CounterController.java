7e6d75a701bb65feb0c2fb7ffda7accc4275afaa
boki <lsb530@naver.com>
2024-07-14 10:27:35 +0900

    AOP - Counted Annotation

--- src/main/java/com/boki/bokispringactuator1/counter/CounterController.java
+++ src/main/java/com/boki/bokispringactuator1/counter/CounterController.java
@@ -1,5 +1,7 @@
 package com.boki.bokispringactuator1.counter;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +16,27 @@ public class CounterController {
 
     private final MyHttpRequestManagerWithoutMicrometer myManager;
 
+    private final MeterRegistry meterRegistry;
+
     @GetMapping("/req")
     public String req() {
 //        myHttpRequestManager.increase();
 
-        myManager.increase(); // filter, interceptor, aop
+//        myManager.increase(); // filter, interceptor, aop
+
+        // these codes to AOP
+        /*Counter.builder("my.http.request")
+            .tag("class", this.getClass().toString())
+            .register(meterRegistry)
+            .increment();
+        */
+
+        return "ok";
+    }
+
+    @Counted("my.counted.counter")
+    @GetMapping("/counted")
+    public String counted() {
         return "ok";
     }
 
