d8586b7bcf25f5b7ffb2ad7ceacc9d67aae3dd62
boki <lsb530@naver.com>
2024-07-14 11:26:00 +0900

    different instance with different tag

--- src/main/java/com/boki/bokispringactuator1/tag/TagController.java
+++ src/main/java/com/boki/bokispringactuator1/tag/TagController.java
@@ -1,6 +1,8 @@
 package com.boki.bokispringactuator1.tag;
 
 import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,8 @@ public class TagController {
 
 //    private final MyQueueManagerWithTags myQueueManagerWithTags;
 
+    private final MeterRegistry meterRegistry;
+
     @Counted(value = "my.counted", extraTags = { "type1", "value1", "type2", "value2" })
     @GetMapping("/push")
     public String push() {
@@ -26,6 +30,31 @@ public class TagController {
     @GetMapping("/pop")
     public String pop() {
 //        myQueueManagerWithTags.pop();
+        return "ok";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        // metrics의 endpoint가 같아도 tag가 다르면 다른 인스턴스가 만들어진다
+        // counter1: -1751949781, counter2: -1751949781 counter3: -1975241815, counter4: -1134301749
+        Counter counter1 = Counter.builder("my.test")
+            .tag("type", "push")
+            .register(meterRegistry);
+
+        Counter counter2 = Counter.builder("my.test")
+            .tag("type", "push")
+            .register(meterRegistry);
+
+        Counter counter3 = Counter.builder("my.test")
+            .tag("type", "push222")
+            .register(meterRegistry);
+
+        Counter counter4 = Counter.builder("my.test")
+            .tag("type", "push333333")
+            .register(meterRegistry);
+
+        log.info("counter1: {}, counter2: {} counter3: {}, counter4: {}", counter1.hashCode(), counter2.hashCode(), counter3.hashCode(), counter4.hashCode());
+
         return "ok";
     }
 }
